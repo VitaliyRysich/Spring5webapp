349ddc420750314c7dc0039ac187349096847d66
vitaliyRysich <vitaliy.rysich@gmail.com>
2019-12-05 23:46:12 +0100

    First commit

---
+++ src/test/java/vitalii/rysich/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package vitalii.rysich.spring5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
